640b865d4bfc70e2242d9a6082a213dee412c64d
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-09-21 08:44:50 +0200

    add/ tests for prune_data

--- tests/unit/test_clean_data.py
+++ tests/unit/test_clean_data.py
@@ -1,14 +1,16 @@
 import pytest
 import pandas as pd
+import numpy as np
+import re
 
-from src.loaders.clean_data import reg_ordinal_encode
+from src.loaders.clean_data import reg_ordinal_encode, prune_data
 
 class TestRegOrdinalEncode():
     def test_raises_typeerror_when_values_is_not_a_list(self, one_row_df):
         with pytest.raises(TypeError):
             reg_ordinal_encode(one_row_df, column="test_column", values="not a list")
             
-    def test_raises_typeerror_when_values_is_not_over_len_2(self, one_row_df):
+    def test_raises_typeerror_when_values_is_not_over_len_2(self):
         with pytest.raises(TypeError):
             reg_ordinal_encode(data=pd.DataFrame({"test_column": [1, 2]}), column="test_column", values=[1])
             
@@ -32,10 +34,46 @@ class TestRegOrdinalEncode():
         df = reg_ordinal_encode(pd.DataFrame({"test_column": [1,2]}), "test_column", values=[1,2])
         assert isinstance(df, pd.DataFrame)
         
-    def test_returns_encoded_column(self, one_row_df):
+    def test_returns_encoded_column(self):
         df = reg_ordinal_encode(pd.DataFrame({
             "test_column": ["medium","low","high"]}), column="test_column", values=["high", "medium", "low"])
         print(df.head())
         assert df["test_column"].iloc[0] == 1
         assert df["test_column"].iloc[1] == 2
         assert df["test_column"].iloc[2] == 0
+
+class TestPruneData:
+    def test_raises_typeerror_when_data_is_not_type_pd_dataframe(self):
+        with pytest.raises(TypeError, match="The `data` parameter must be a pandas DataFrame `pd.DataFrame`"):
+            prune_data("not a dataframe", ["test_column"])
+    
+    def test_raises_typerror_when_columns_not_type_list(self):
+        with pytest.raises(TypeError, match="The `columns` parameter must be a list"):
+            prune_data(data=pd.DataFrame({"test_column": [1,2]}), columns="test_column")
+    
+    def test_raises_valueerror_when_columns_is_empty_list(self):
+        with pytest.raises(ValueError, match="The `columns` parameter must be a non-empty list"):
+            prune_data(data=pd.DataFrame({"test_column": [1,2]}), columns=[])
+
+    def test_raises_keyerror_when_column_not_in_df(self):
+        with pytest.raises(KeyError, match=re.escape("Column(s) not found in the `data` dataframe:")):
+            prune_data(data=pd.DataFrame({"test_column": [1,2]}), columns=["test_column", "not_in_df"])
+
+    def test_raises_valueerror_on_empty_dataframe(self):
+        with pytest.raises(ValueError, match="The `data` dataframe has no rows"):
+            prune_data(data=pd.DataFrame({}), columns=["test_column"])
+
+    def test_returns_pd_dataframe(self):
+        assert isinstance(prune_data(data=pd.DataFrame({"test_column": [1,2]}), columns=["test_column"]), pd.DataFrame)
+    
+    def test_returns_pd_dataframe_with_pruned_columns(self):
+        df = prune_data(data=pd.DataFrame({"test_column": [1,2], "test_column_2":[1,2]}), columns=["test_column"])
+        assert df.shape == (2, 1)
+        column_in_list = False
+        if "test_column_2" in df.columns.to_list():
+            column_in_list = True
+        assert column_in_list == False
+    
+    def test_returns_pd_dataframe_without_na_values(self):
+        df = prune_data(data=pd.DataFrame({"test_column": [1,np.nan], "test_column_2":[1,2]}), columns=["test_column"])
+        assert int(df.isnull().sum().iloc[0]) == 0

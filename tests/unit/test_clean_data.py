9822ca1b1a70b71c6fe3c17cb4d1eae8ece68efd
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-09-21 08:16:51 +0200

    add/ tests for reg_ordinal_encode

--- tests/unit/test_clean_data.py
+++ tests/unit/test_clean_data.py
@@ -23,4 +23,19 @@ class TestRegOrdinalEncode():
     def test_raises_keyerror_when_column_not_in_data(self, one_row_df):
         with pytest.raises(KeyError):
             reg_ordinal_encode(one_row_df, "test_column", values=[1,2,3])
+            
+    def test_raises_valueerror_when_column_is_empty(self):
+        with pytest.raises(ValueError):
+            reg_ordinal_encode(pd.DataFrame({"test_column": []}), "test_column", values=[1,2])
     
+    def test_returns_pd_dataframe(self):
+        df = reg_ordinal_encode(pd.DataFrame({"test_column": [1,2]}), "test_column", values=[1,2])
+        assert isinstance(df, pd.DataFrame)
+        
+    def test_returns_encoded_column(self, one_row_df):
+        df = reg_ordinal_encode(pd.DataFrame({
+            "test_column": ["medium","low","high"]}), column="test_column", values=["high", "medium", "low"])
+        print(df.head())
+        assert df["test_column"].iloc[0] == 1
+        assert df["test_column"].iloc[1] == 2
+        assert df["test_column"].iloc[2] == 0

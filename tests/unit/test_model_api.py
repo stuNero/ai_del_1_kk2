da8034a4d99dbf66cf0c1a29dc8fdd030b7dfdd7
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-09-21 10:03:27 +0200

    add/ tests for load_regression_model

--- tests/unit/test_model_api.py
+++ tests/unit/test_model_api.py
@@ -1,8 +1,10 @@
 import pytest
+from unittest.mock import patch, MagicMock
 import re
 import sqlite3
 from contextlib import closing
-from src.api.model_api import fetch_model
+from src.api.model_api import fetch_model, load_regression_model
+from sklearn.pipeline import Pipeline
 
 class TestFetchModel:
     def test_raises_valueerror_when_model_type_is_invalid(self,db_path):
@@ -18,3 +20,15 @@ class TestFetchModel:
     def test_raises_connectionerror_if_connection_fails(self,db_path):
         with pytest.raises(ConnectionError):
             fetch_model("regression", db_path=db_path)
+
+class TestLoadRegressionModel:
+    def test_calls_fetch_model_with_regression_type(self):
+        with patch("src.api.model_api.fetch_model") as mock_fetch_model:
+            mock_fetch_model.return_value = MagicMock(spec=Pipeline)
+            
+            result = load_regression_model(db_path="some/path.db")
+            
+            mock_fetch_model.assert_called_once_with(
+                model_type="regression", db_path="some/path.db"
+            )
+            assert result == mock_fetch_model.return_value

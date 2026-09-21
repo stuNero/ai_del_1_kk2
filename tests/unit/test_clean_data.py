28f5e3d3bdae460887fd5e4fa9bc5e3f6dd39251
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-09-21 07:53:00 +0200

    add/ tests for reg_ordinal_encode

---
+++ tests/unit/test_clean_data.py
@@ -0,0 +1,26 @@
+import pytest
+import pandas as pd
+
+from src.loaders.clean_data import reg_ordinal_encode
+
+class TestRegOrdinalEncode():
+    def test_raises_typeerror_when_values_is_not_a_list(self, one_row_df):
+        with pytest.raises(TypeError):
+            reg_ordinal_encode(one_row_df, column="test_column", values="not a list")
+            
+    def test_raises_typeerror_when_values_is_not_over_len_2(self, one_row_df):
+        with pytest.raises(TypeError):
+            reg_ordinal_encode(data=pd.DataFrame({"test_column": [1, 2]}), column="test_column", values=[1])
+            
+    def test_raises_typeerror_when_data_is_not_type_pd_dataframe(self):
+        with pytest.raises(TypeError):
+            reg_ordinal_encode("not a dataframe", "test_column", values=[1,2,3])
+            
+    def test_raises_typeerror_when_column_is_not_type_str(self, one_row_df):
+        with pytest.raises(TypeError):
+            reg_ordinal_encode(one_row_df, 1, values=[1,2,3])
+            
+    def test_raises_keyerror_when_column_not_in_data(self, one_row_df):
+        with pytest.raises(KeyError):
+            reg_ordinal_encode(one_row_df, "test_column", values=[1,2,3])
+    
